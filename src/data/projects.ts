01f5db1b48ebfa3ae91ae26e20b650cc6a23c2c6
ngimdock <ngimdockzemfack@gmail.com>
2023-07-17 10:09:50 +0100

    update DB

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -317,98 +317,98 @@ export const ALL_PROJECTS: Project[] = [
     images: [],
   },
 
-  {
-    id: 5,
-    featured: 1,
-    name: "Vision Car.",
-    description:
-      "API Rest complete pour une application de ventes et gestion des voitures de luxe.",
-    mainImage: "images.jpeg",
-    tags: [ingenirie, projetsSolo, backend],
-    infos: {
-      industrie: "E-commerce",
-      type: ProjectType.WEB_APP,
-      publishDate: "22 janvier 2022",
-      github: "https://github.com/ngimdock/vision-car-server",
-      reasonForNoLink: reasonLink.notOnline,
-    },
-    details: {
-      problem: `Je souhaite approfondir mes compétences et mettre en pratique l'ensembles 
-      des nouvelles technologies que j'ai apprise. Je veux m'assurer de développer une 
-      application qui contient des fonctionnalités très avancés et qui peut être utilisé
-      dans le monde réel. Une application dont je vais m'occuper de toutes les phase les plusimportantes 
-      du développement d'un logiciel: Analyse des besoin, conception, design, développement, test, documentation,
-      deploiement.`,
-      solution: `Pour y arriver, je me suis crée une entreprise fictive au nom de vision-car qui vend 
-      des voitures de luxe. j'ai rescencé les fonctionnalités les plus importantes qu'une application 
-      similaire aurait besoin, gestion de l'authentification, gestion des rôles des acteurs du système, 
-      restreindre les access aux ressources de l'API avec des authorization, gestion de tous les acteurs, 
-      commmandes, factures. Gestion de l'intéractivités entre les acteurs. tests unitaires et end to end, `,
-      myTasks: `après j'ai réalisé la conception qui m'a permit de deduire le schema 
-        de la BD. Ensuite je me suis mis à développer le backend du projet. En parallele du développement du backend, 
-        je réalisait les maquettes du projet.`,
-      techStack: [nestjs, reactjs, postgres, prisma, stripe, jest],
-    },
-    images: ["contact-call-to-action.jpg", "dan.png"],
-    challenges: [
-      {
-        challengeTitle: "Premier challenge",
-        challengeDescription:
-          "Un des plus grand challenge sur ce projet à été d'éffectuer une bonne conception de la base de donnée.",
-        solutionTitle: "Solution de conception",
-        solutionDescription:
-          "J'ai réalisé un MCD(Modèle Conceptuel de Donnée) évolutif que j'ai pu mettre en place après avoir bien définir les besoins du projet. Le schema de la BD est très adapté s'adapte facilement à des mises à jour.",
-        solutionImage: "contact-call-to-action.jpg",
-      },
-    ],
-  },
-  {
-    id: 6,
-    featured: 4,
-    name: "Business-Teach",
-    description: "Une application pour trouver des répétiteurs au Cameroun.",
-    mainImage: "images.jpeg",
-    tags: [Tags.applicationWeb, Tags.projetDequipe, Tags.frontend],
-    infos: {
-      industrie: "Enseignement",
-      type: ProjectType.WEB_APP,
-      publishDate: "23 janvier 2022",
-      link: "https://bteach-server.web.app/",
-      github: "https://github.com/ngimdock/bteach",
-    },
-    details: {
-      problem:
-        "Le point de départ de ce projet était la participation à un programme de développement organisé par Google, où les participants devaient développer une application pour résoudre un problème concret dans leur pays.",
-      solution:
-        "Nous avons choisi d'intervenir dans le domaine de l'enseignement en créant une plateforme permettant aux personnes de trouver des professeurs particuliers sur mesure, en fonction de leur profil et  qualification.",
-      myTasks:
-        "Mon intervention principale a été durant la phase de développement des fonctionnalités, ainsi que la réalisation et l'intégration des maquettes.",
-      techStack: [reactjs, tailwind, firebase],
-    },
-    images: [],
-  },
+  // {
+  //   id: 5,
+  //   featured: 1,
+  //   name: "Vision Car.",
+  //   description:
+  //     "API Rest complete pour une application de ventes et gestion des voitures de luxe.",
+  //   mainImage: "images.jpeg",
+  //   tags: [ingenirie, projetsSolo, backend],
+  //   infos: {
+  //     industrie: "E-commerce",
+  //     type: ProjectType.WEB_APP,
+  //     publishDate: "22 janvier 2022",
+  //     github: "https://github.com/ngimdock/vision-car-server",
+  //     reasonForNoLink: reasonLink.notOnline,
+  //   },
+  //   details: {
+  //     problem: `Je souhaite approfondir mes compétences et mettre en pratique l'ensembles
+  //     des nouvelles technologies que j'ai apprise. Je veux m'assurer de développer une
+  //     application qui contient des fonctionnalités très avancés et qui peut être utilisé
+  //     dans le monde réel. Une application dont je vais m'occuper de toutes les phase les plusimportantes
+  //     du développement d'un logiciel: Analyse des besoin, conception, design, développement, test, documentation,
+  //     deploiement.`,
+  //     solution: `Pour y arriver, je me suis crée une entreprise fictive au nom de vision-car qui vend
+  //     des voitures de luxe. j'ai rescencé les fonctionnalités les plus importantes qu'une application
+  //     similaire aurait besoin, gestion de l'authentification, gestion des rôles des acteurs du système,
+  //     restreindre les access aux ressources de l'API avec des authorization, gestion de tous les acteurs,
+  //     commmandes, factures. Gestion de l'intéractivités entre les acteurs. tests unitaires et end to end, `,
+  //     myTasks: `après j'ai réalisé la conception qui m'a permit de deduire le schema
+  //       de la BD. Ensuite je me suis mis à développer le backend du projet. En parallele du développement du backend,
+  //       je réalisait les maquettes du projet.`,
+  //     techStack: [nestjs, reactjs, postgres, prisma, stripe, jest],
+  //   },
+  //   images: ["contact-call-to-action.jpg", "dan.png"],
+  //   challenges: [
+  //     {
+  //       challengeTitle: "Premier challenge",
+  //       challengeDescription:
+  //         "Un des plus grand challenge sur ce projet à été d'éffectuer une bonne conception de la base de donnée.",
+  //       solutionTitle: "Solution de conception",
+  //       solutionDescription:
+  //         "J'ai réalisé un MCD(Modèle Conceptuel de Donnée) évolutif que j'ai pu mettre en place après avoir bien définir les besoins du projet. Le schema de la BD est très adapté s'adapte facilement à des mises à jour.",
+  //       solutionImage: "contact-call-to-action.jpg",
+  //     },
+  //   ],
+  // },
+  // {
+  //   id: 6,
+  //   featured: 4,
+  //   name: "Business-Teach",
+  //   description: "Une application pour trouver des répétiteurs au Cameroun.",
+  //   mainImage: "images.jpeg",
+  //   tags: [Tags.applicationWeb, Tags.projetDequipe, Tags.frontend],
+  //   infos: {
+  //     industrie: "Enseignement",
+  //     type: ProjectType.WEB_APP,
+  //     publishDate: "23 janvier 2022",
+  //     link: "https://bteach-server.web.app/",
+  //     github: "https://github.com/ngimdock/bteach",
+  //   },
+  //   details: {
+  //     problem:
+  //       "Le point de départ de ce projet était la participation à un programme de développement organisé par Google, où les participants devaient développer une application pour résoudre un problème concret dans leur pays.",
+  //     solution:
+  //       "Nous avons choisi d'intervenir dans le domaine de l'enseignement en créant une plateforme permettant aux personnes de trouver des professeurs particuliers sur mesure, en fonction de leur profil et  qualification.",
+  //     myTasks:
+  //       "Mon intervention principale a été durant la phase de développement des fonctionnalités, ainsi que la réalisation et l'intégration des maquettes.",
+  //     techStack: [reactjs, tailwind, firebase],
+  //   },
+  //   images: [],
+  // },
 
-  {
-    id: 7,
-    name: "Becon website",
-    description:
-      "Une application web et mobile pour dématérialiser la délivrance des cartes nationales d'identités(cni) du cameroun.",
-    mainImage: "images.jpeg",
-    tags: [Tags.projetsSolo],
-    infos: {
-      industrie: "Informatique",
-      type: ProjectType.SHOWCASE_WEBSITE,
-      publishDate: "22 janvier 2022",
-      link: "https://beacon-website-302ee.web.app/",
-      reasonForNoGithub: reasonGithub.privateCode,
-    },
-    details: {
-      problem: "The problem here",
-      solution: "The sution here",
-      myTasks: "My task here",
-      techStack: [reactjs, tailwind, firebase],
-      team: "Team details",
-    },
-    images: [],
-  },
+  // {
+  //   id: 7,
+  //   name: "Becon website",
+  //   description:
+  //     "Une application web et mobile pour dématérialiser la délivrance des cartes nationales d'identités(cni) du cameroun.",
+  //   mainImage: "images.jpeg",
+  //   tags: [Tags.projetsSolo],
+  //   infos: {
+  //     industrie: "Informatique",
+  //     type: ProjectType.SHOWCASE_WEBSITE,
+  //     publishDate: "22 janvier 2022",
+  //     link: "https://beacon-website-302ee.web.app/",
+  //     reasonForNoGithub: reasonGithub.privateCode,
+  //   },
+  //   details: {
+  //     problem: "The problem here",
+  //     solution: "The sution here",
+  //     myTasks: "My task here",
+  //     techStack: [reactjs, tailwind, firebase],
+  //     team: "Team details",
+  //   },
+  //   images: [],
+  // },
 ];
